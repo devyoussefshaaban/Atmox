4226762760eba8f66d6d182e41408ea5690425b5
unknown <imdevyoussef@gmail.com>
2025-12-08 14:52:24 +0200

    home ui - main ui implementation

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,22 @@
+import Image from "next/image";
+
 const Home = () => {
   return (
-    <div>
-      <button className="bg-primary  px-4 py-2">
-        Button
-      </button>
-    </div>
+    <main className=" mt-16">
+      <h1 className="font-bricolage font-bolder text-center text-5xl mb-12">How is the sky looking today?</h1>
+      <div className="max-w-xl mx-auto">
+        <div className="flex justify-center gap-2 wrap mx-auto w-full">
+          <div className="w-full rounded-lg border-none bg-gray-800 text-md  px-6 py-3 flex items-center">
+            <Image width={12} height={15} src="/images/icon-search.svg" alt="Search Icon" className="mr-2" />
+            <input type="text" className="w-full border-none bg-gray-800 text-md outline-none"
+              placeholder="Search for a place ..." />
+          </div>
+          <div>
+            <button className="rounded-lg bg-blue-500 px-8 py-3 font-semibold cursor-pointer text-white">Search</button>
+          </div>
+        </div>
+      </div>
+    </main >
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,40 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}", // Next.js 13+ App Router
-    "./pages/**/*.{js,ts,jsx,tsx}", // For Pages Router
+    "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
+      fontFamily: {
+        dmSans: ["var(--font-dmSans)", "sans-serif"],
+        bricolage: ["var(--font-bricolage)", "sans-serif"],
+      },
       colors: {
-        primary: "red",
-        secondary: "#f59e0b",
+        neutral: {
+          900: "hsl(243, 96%, 9%)",
+          800: "hsl(243, 27%, 20%)",
+          700: "hsl(243, 23%, 24%)",
+          600: "hsl(243, 23%, 30%)",
+          300: "hsl(240, 6%, 70%)",
+          200: "hsl(250, 6%, 84%)",
+          0: "hsl(0, 0%, 100%)",
+        },
+        orange: {
+          500: "hsl(28, 100%, 52%)",
+        },
+        blue: {
+          500: "hsl(233, 67%, 56%)",
+          700: "hsl(248, 70%, 36%)",
+        },
+      },
+      fontSize: {
+        base: "18px",
+      },
+      screens: {
+        mobile: "375px",
+        desktop: "1440px",
       },
     },
   },
